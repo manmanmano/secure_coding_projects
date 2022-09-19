c5a846f67408e0ad3c8fb73130411da2a7e32224
manmanmano <jimiskools@gmail.com>
2022-09-19 10:22:47 +0300

    positions

--- Crypto2022/CryptoDemo22f/ConsoleApp01/Program.cs
+++ Crypto2022/CryptoDemo22f/ConsoleApp01/Program.cs
@@ -7,8 +7,29 @@ string defaultShiftAmount = "3";
 
 var alphabet = Helper.GetUserAlphabet(defaultAlphabet);
 
-Console.WriteLine(alphabet);
-
 var shiftAmount = Helper.CheckNumbersInInput(defaultShiftAmount, alphabet.Length);
 
-Console.WriteLine(shiftAmount);
+Console.Write("Plaintext: ");
+var plainText = Console.ReadLine();
+var plainTextFixed = "";
+
+if (plainText != null)
+{
+    foreach (var chr in plainText)
+    {
+        if (alphabet.Contains(chr))
+        {
+            plainTextFixed += chr;
+        }
+    }
+    Console.WriteLine("Fixed text: " + plainTextFixed);
+
+    var encryptedText = "";
+    foreach (var chr in plainTextFixed)
+    {
+        var pos = alphabet.IndexOf(chr) + shiftAmount % alphabet.Length;
+        pos = pos < 0 ? pos + alphabet.Length : pos;
+        encryptedText += alphabet[pos];
+    }
+    Console.WriteLine("Encrypted text: " + encryptedText);
+}
