58189f09bfa30fdf26f3049c4bb4cfd45175e76d
Mariano <madang@taltech.ee>
2022-09-21 19:05:08 +0300

    shift

--- Crypto2022/CryptoDemo22f/ConsoleAppCaesarUTF/Program.cs
+++ Crypto2022/CryptoDemo22f/ConsoleAppCaesarUTF/Program.cs
@@ -13,3 +13,13 @@ foreach (var b in plainTextBytes)
 {
     Console.WriteLine(b);
 }
+
+// shift the bytes
+var b64Text = System.Convert.ToBase64String(plainTextBytes);
+Console.WriteLine("B64 text: " + b64Text);
+
+// unshift the bytes
+var decryptedBytes = System.Convert.FromBase64String(b64Text);
+var decryptedText = System.Text.Encoding.UTF8.GetString(decryptedBytes);
+
+Console.WriteLine("decrypted text: " + decryptedText);
