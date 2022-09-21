2aa52ad0e07da8f5ffe2576bf0b543ca722893d5
Mariano <madang@taltech.ee>
2022-09-21 18:53:32 +0300

    caesar utf

---
+++ Crypto2022/CryptoDemo22f/ConsoleAppCaesarUTF/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+Console.Write("Shift amount: ");
+var amount = int.Parse(Console.ReadLine() ?? string.Empty);
+
+Console.Write("Plain text: ");
+var plaintext = Console.ReadLine();
+
+var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plaintext!);
+foreach (var b in plainTextBytes)
+{
+    Console.WriteLine(b);
+}
