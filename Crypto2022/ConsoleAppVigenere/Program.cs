608a862acc4ff73d0509694275fc482a9067436c
Mariano D'Angelo <madang@taltech.ee>
2022-09-25 01:39:41 +0300

    starting out vigenere, only testing done

---
+++ Crypto2022/ConsoleAppVigenere/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Plaintext: ");
+var plaintext = Console.ReadLine();
+Console.Write("Passphrase: ");
+var passphrase = Console.ReadLine();
+
+var plaintextBytes = System.Text.Encoding.UTF8.GetBytes(plaintext!);
+Console.Write("Bytes for plaintext : ");
+foreach (var b in plaintextBytes)
+{
+    Console.Write(b + " ");
+}
+
+var passphraseBytes = System.Text.Encoding.UTF8.GetBytes(passphrase!);
+Console.Write("\nBytes for plaintext : ");
+foreach (var b in passphraseBytes)
+{
+    Console.Write(b + " ");
+}
