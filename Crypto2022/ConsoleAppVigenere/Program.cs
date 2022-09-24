c1516cbb9481f5754b744a0d62d17a6f9d103b50
Mariano D'Angelo <madang@taltech.ee>
2022-09-25 02:51:49 +0300

    Counting bytes

--- Crypto2022/ConsoleAppVigenere/Program.cs
+++ Crypto2022/ConsoleAppVigenere/Program.cs
@@ -6,21 +6,11 @@ var plaintext = Helper.GetPlaintext();
 Console.Write("Passphrase: ");
 var passphrase = Helper.GetPassphrase();
 
-/*
-Console.Write("Passphrase: ");
-var passphrase = Console.ReadLine();
-
 var plaintextBytes = System.Text.Encoding.UTF8.GetBytes(plaintext!);
-Console.Write("Bytes for plaintext : ");
-foreach (var b in plaintextBytes)
-{
-    Console.Write(b + " ");
-}
+Console.Write("Bytes for plaintext: ");
+foreach (var b in plaintextBytes) Console.Write(b + " ");
 
 var passphraseBytes = System.Text.Encoding.UTF8.GetBytes(passphrase!);
-Console.Write("\nBytes for plaintext : ");
-foreach (var b in passphraseBytes)
-{
-    Console.Write(b + " ");
-}
-*/
+Console.Write("\nBytes for passphrase: ");
+foreach (var b in passphraseBytes) Console.Write(b + " ");
+
