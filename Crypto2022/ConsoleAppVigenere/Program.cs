937fa40336e30876fda84969cd748b6159bfef46
Mariano D'Angelo <madang@taltech.ee>
2022-09-25 15:43:55 +0200

    ENCRYPTION and DECRYPTION done. Output prettified. HOMEWORK 2 FINISHED

--- Crypto2022/ConsoleAppVigenere/Program.cs
+++ Crypto2022/ConsoleAppVigenere/Program.cs
@@ -9,7 +9,7 @@ Console.Write("Passphrase: ");
 var passphrase = Helper.GetPassphrase();
 
 var plaintextBytes = System.Text.Encoding.UTF8.GetBytes(plaintext!);
-Console.Write("Plaintext bytes: ");
+Console.Write("\nPlaintext bytes: ");
 // print out bytes of plaintext
 foreach (var b in plaintextBytes) Console.Write(b + " ");
 
@@ -31,13 +31,36 @@ Console.Write("\nPassphrase bytes after validation: ");
 // print out bytes of new validated passphrase
 foreach (var b in passphraseInts) Console.Write(b + " ");
 
+
+// ENCRYPTION PHASE
+
 var shiftedBytes = new byte[plaintextBytes.Length];
+// encrypt with Vigenere's formula
 for (int i = 0; i < plaintextBytes.Length; i++)
 {
     shiftedBytes[i] = (byte)((plaintextBytes[i] + passphraseInts[i]) % 255);
 }
 // print out new bytes of shifted value
-Console.Write("\nPlaintext bytes after Vigenere encryption: ");
+Console.Write("\n\nPlaintext bytes after Vigenere encryption: ");
 foreach (var b in shiftedBytes) Console.Write(b + " ");
 
+// encode the encrypted plaintext to base64 and print out
 var b64Text = System.Convert.ToBase64String(shiftedBytes);
+Console.Write("\nThe encrypted plaintext: " + b64Text);
+
+
+// DECRYPTION PHASE
+
+// get back unencoded bytes
+var unencodedShiftedBytes = System.Convert.FromBase64String(b64Text);
+// unshift/decrypt bytes with Vigenere's formula
+for (int i = 0; i < shiftedBytes.Length; i++)
+{
+    unencodedShiftedBytes[i] = (byte)((unencodedShiftedBytes[i] - passphraseInts[i] + 255) % 255);
+}
+Console.Write("\n\nPlaintext bytes after Vigenere decryption: ");
+foreach (var b in unencodedShiftedBytes) Console.Write(b + " ");
+
+// decrypt bytes to text
+var decryptedText = System.Text.Encoding.UTF8.GetString(unencodedShiftedBytes);
+Console.WriteLine("\nThe decrypted plaintext: " + decryptedText);
