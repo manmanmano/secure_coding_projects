ff1d3196c7aabc4f111c9fac2d59a9a9bb59932b
Mariano D'Angelo <madang@taltech.ee>
2022-09-27 16:31:26 +0200

    biginteger

--- Crypto2022/ConsoleAppDH/Program.cs
+++ Crypto2022/ConsoleAppDH/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Hello, Diffie Hellman!");
+﻿using System.Numerics;
+
+Console.WriteLine("Hello, Diffie Hellman!");
 
 // TODO: Check that it is a prime
 Console.Write("Public key P (prime): ");
@@ -14,14 +16,18 @@ Console.Write("Person Y private key B: ");
 var b = int.Parse(Console.ReadLine()?.Trim());
 
 // TODO: memory overflow (int cannot contain 5^15)
-var computeX = (long)Math.Pow(g, a) % p;
+// var computeX = (long)Math.Pow(g, a) % p;
+var computeX = (long)BigInteger.ModPow(g, a, p);
 Console.WriteLine($"Person X computed: {computeX}");
 
-var computeY = (long)Math.Pow(g, b) % p;
+// var computeY = (long)Math.Pow(g, b) % pa;
+var computeY = (long)BigInteger.ModPow(g, b, p);
 Console.WriteLine($"Person Y computed: {computeY}");
 
-var computeX2= (long)Math.Pow(g, b) % p;
+// var computeX2= (long)Math.Pow(g, b) % p;
+var computeX2 = (long)BigInteger.ModPow(computeY, a, p);
 Console.WriteLine($"Person X received {computeY} computed: {computeX2}");
 
-var computeY2= (long)Math.Pow(g, b) % p;
+// var computeY2= (long)Math.Pow(g, b) % p;
+var computeY2 = (long)BigInteger.ModPow(computeX, a, p);
 Console.WriteLine($"PersonX received {computeX} computed: {computeY2}");
