f12fc48acaadcd7e2b0a6f6f5797b29127006ef2
Mariano D'Angelo <madang@taltech.ee>
2022-10-04 16:00:45 +0200

    Start. Add functions CheckPrime and ValidateInput. Write down codes to differentiate output in ValidateInput

--- Crypto2022/ConsoleAppDH/Helper.cs
+++ Crypto2022/ConsoleAppDH/Helper.cs
@@ -2,5 +2,38 @@ namespace ConsoleAppDH;
 
 public class Helper
 {
+    public static int ValidateInput(int functionCode)
+    { 
+        Console.WriteLine("Input must be an integer!");
+        bool isInt;
+        string? input;
+        int p;
+        do
+        {
+            isInt = true;
+            switch (functionCode)
+            {
+                case 0:
+                    Console.Write("Public key P (prime) [generate randomly]: ");
+                    break;
+            }
+            input = Console.ReadLine()?.Trim();
+            if (!int.TryParse(input, out p))
+            {
+                Console.WriteLine("Input must be an integer!");
+                isInt = false;
+            }
+        } while (isInt == false);
+        return p;
+    }
     
+    
+    public static int CheckPrime(int prime)
+    {
+        var input = 0;
+        if (prime == 0)
+        {
+        }
+        return 0;
+    }
 }
--- Crypto2022/ConsoleAppDH/Program.cs
+++ Crypto2022/ConsoleAppDH/Program.cs
@@ -1,40 +1,50 @@
 ﻿using System.Numerics;
+using ConsoleAppDH;
+
+// codes used in input validation function to differentiate output
+int pCode = 0, gCode = 1, pXCode = 2, pYCode = 3;
 
 Console.WriteLine("Hello, Diffie Hellman!");
 
+Console.Write("Public key P (prime) [generate randomly]: ");
+int p;
+var isPrime = int.TryParse(Console.ReadLine()?.Trim(), out p);
+if (!isPrime)
+{
+    p = Helper.ValidateInput(pCode);
+}
 
 // TODO: Check, that it is a prime
 // TODO: Generate for user the biggest prime under user specified value
-Console.Write("Public key P (prime):");
-var p = int.Parse(Console.ReadLine()?.Trim());
+p = Helper.CheckPrime(p);
 
-Console.Write("Public key G (base number):");
-var g = int.Parse(Console.ReadLine()?.Trim());
-
-Console.Write("PersonX private key A:");
-var a = int.Parse(Console.ReadLine()?.Trim());
-
-Console.Write("PersonY private key B:");
-var b = int.Parse(Console.ReadLine()?.Trim());
-
-// TODO: memory overflow (int cannot contain 5^15) 
-// var computeX =  (long)Math.Pow(g, a)  % p;
-// TODO: implement your own ModPow, using long/int
-var computeX = (long) BigInteger.ModPow(g, a,p);
-Console.WriteLine($"PersonX computed: {computeX}");
-
-// var computeY =  (long)Math.Pow(g, b)  % p;
-var computeY = (long) BigInteger.ModPow(g, b,p);
-
-Console.WriteLine($"PersonY computed: {computeY}");
-
-// var computeX2 = (long) Math.Pow(computeY, a) % p;
-var computeX2 = (long) BigInteger.ModPow(computeY, a, p);
-Console.WriteLine($"PersonX received {computeY} and computed: {computeX2}");
-
-// var computeY2 = (long) Math.Pow(computeX, b) % p;
-var computeY2 = (long) BigInteger.ModPow(computeX, b, p);
-Console.WriteLine($"PersonY received {computeX} and computed: {computeY2}");
+// Console.Write("Public key G (base number): ");
+// var g = int.Parse(Console.ReadLine()?.Trim());
+// 
+// Console.Write("PersonX private key A: ");
+// var a = int.Parse(Console.ReadLine()?.Trim());
+// 
+// Console.Write("PersonY private key B: ");
+// var b = int.Parse(Console.ReadLine()?.Trim());
+// 
+// // TODO: memory overflow (int cannot contain 5^15) 
+// // var computeX =  (long)Math.Pow(g, a)  % p;
+// // TODO: implement your own ModPow, using long/int
+// var computeX = (long) BigInteger.ModPow(g, a,p);
+// Console.WriteLine($"PersonX computed: {computeX}");
+// 
+// // var computeY =  (long)Math.Pow(g, b)  % p;
+// var computeY = (long) BigInteger.ModPow(g, b,p);
+// 
+// Console.WriteLine($"PersonY computed: {computeY}");
+// 
+// // var computeX2 = (long) Math.Pow(computeY, a) % p;
+// var computeX2 = (long) BigInteger.ModPow(computeY, a, p);
+// Console.WriteLine($"PersonX received {computeY} and computed: {computeX2}");
+// 
+// // var computeY2 = (long) Math.Pow(computeX, b) % p;
+// var computeY2 = (long) BigInteger.ModPow(computeX, b, p);
+// Console.WriteLine($"PersonY received {computeX} and computed: {computeY2}");
 
 
 // TODO: Prime number control
