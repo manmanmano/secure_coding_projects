ea4eec5fa23dc9e67f72bb868fd638ca15b9e558
Mariano D'Angelo <madang@taltech.ee>
2022-11-30 15:31:26 +0100

    remove redundant cde

--- Web2022/WebApp/Helpers/CaesarHelper.cs
+++ Web2022/WebApp/Helpers/CaesarHelper.cs
@@ -13,15 +13,4 @@ public class CaesarHelper
         
         return System.Convert.ToBase64String(encryptedBytes);
     }
-
-    public static byte[] UnshiftBytes(byte[] shiftedBytes, int shiftAmount)
-    {
-        var unshiftedBytes = new byte[shiftedBytes.Length];
-        for (int i = 0; i < shiftedBytes.Length; i++)
-        {
-            unshiftedBytes[i] = (byte)(shiftedBytes[i] - shiftAmount % 255);
-        }
-
-        return unshiftedBytes;
-    }
 }
